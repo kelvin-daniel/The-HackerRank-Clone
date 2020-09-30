04861ebd29440ac9d37b7eee396eb3e8d5ccf032
kelvin-daniel <kaymutor@gmail.com>
2020-09-30 14:28:42 +0300

    setup email configurations

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
+import os
+
 class Config():
     '''
     General app configuration 
     '''
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/hackerrank'
-
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class ProdConfig(Config):
     pass
