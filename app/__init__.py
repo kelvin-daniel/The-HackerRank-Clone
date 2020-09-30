e87e456e12d2cbcfd8bfd244beea6b35b4679d05
kelvin-daniel <kaymutor@gmail.com>
2020-09-30 14:32:31 +0300

    initialize email on fire up

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config_options
+from flask_mail import Mail
 
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -14,6 +16,7 @@ def create_app(config_name):
 
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
    
 
     # Registering blueprints
