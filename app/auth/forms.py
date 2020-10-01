2a89e69cb1444b4459d672cf21c1e648ca490325
kelvin-daniel <kaymutor@gmail.com>
2020-10-02 00:22:03 +0300

    fix errors on render templates

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField ,SubmitField,TextAreaField
-from wtforms.validators import Required
+from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
 
